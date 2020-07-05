2d1947218096ee2cbc994e076faaf8699a380f36
yu nie <nieyuchina@gmail.com>
2020-07-05 18:16:42 +0800

    first init

---
+++ testSwiftCommon/testSwiftCommon.h
@@ -0,0 +1,19 @@
+//
+//  testSwiftCommon.h
+//  testSwiftCommon
+//
+//  Created by nie yu on 2020/7/5.
+//  Copyright © 2020 nie yu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for testSwiftCommon.
+FOUNDATION_EXPORT double testSwiftCommonVersionNumber;
+
+//! Project version string for testSwiftCommon.
+FOUNDATION_EXPORT const unsigned char testSwiftCommonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <testSwiftCommon/PublicHeader.h>
+
+
