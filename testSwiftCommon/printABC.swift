b9ec34b236fa52ba6e1142c17e71b3182c1775b8
yu nie <nieyuchina@gmail.com>
2020-07-05 18:38:24 +0800

    second commit

---
+++ testSwiftCommon/printABC.swift
@@ -0,0 +1,17 @@
+//
+//  printABC.swift
+//  testSwiftCommon
+//
+//  Created by nie yu on 2020/7/5.
+//  Copyright © 2020 nie yu. All rights reserved.
+//
+
+import Foundation
+
+public class printABC: NSObject {
+    
+    public class func printABC() {
+        print("abc")
+    }
+    
+}
